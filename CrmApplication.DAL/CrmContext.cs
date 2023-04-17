cd6d7cbb55c9c9fda4c90612f0db98e83488d67e
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-04-17 14:15:00 +0300

    Add project files.

---
+++ CrmApplication.DAL/CrmContext.cs
@@ -0,0 +1,23 @@
+﻿using CrmApplication.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmApplication.DAL
+{
+    public class CrmContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("server=DESKTOP-M8JLN9L\\SQLEXPRESS;database=AcunMedyaCrm");
+            }
+        }
+    }
+}
---
+++ CrmApplication.Entites/Customer.cs
@@ -0,0 +1,20 @@
+﻿using CrmApplication.Entites.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmApplication.Entites
+{
+    [Table("Customers")]
+    public class Customer :EntityBase
+    {
+        public string CompanyName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? TaxAdmin { get; set; }
+        public string? TaxNumber { get; set; }
+    }
+}
