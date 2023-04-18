b56eee74407cfe99f87b24e0881ab1be3d2ed4ed
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-04-18 16:06:20 +0300

    Admin template eklendi popup ile create edit ve delete alanları tamamlandı.

--- CrmApplication.UI/Controllers/CustomerController.cs
+++ CrmApplication.UI/Controllers/CustomerController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CrmApplication.BLL;
+using CrmApplication.DAL.EntitiyFramework;
+using CrmApplication.Entites;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CrmApplication.UI.Controllers
 {
     public class CustomerController : Controller
     {
+        CustomerManager customerManager = new CustomerManager(new EfCustomerRepository());
         public IActionResult Index()
         {
-            return View();
+            List<Customer> list = customerManager.ListAll();
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            customerManager.Create(customer);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult EditCustomerPartial(int id)
+        {
+            Customer customer = customerManager.Get(id);
+            return PartialView("_CustomerEditPartialView", customer);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            customerManager.Update(customer);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult DeleteCustomerPartial(int id)
+        {
+            Customer customer = customerManager.Get(id);
+            return PartialView("_CustomerDeletePartialView", customer);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Customer customer)
+        {
+            customerManager.Delete(customer);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- CrmApplication.UI/Program.cs
+++ CrmApplication.UI/Program.cs
@@ -7,7 +7,7 @@ namespace CrmApplication.UI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             var app = builder.Build();
 
