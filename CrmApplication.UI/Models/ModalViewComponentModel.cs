4e5e1844d233b89493e8007a5a05809d601c3072
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-04-24 16:17:09 +0300

    Generic modal yapıldı

---
+++ CrmApplication.UI/Models/ModalViewComponentModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CrmApplication.UI.Models
+{
+    public class ModalViewComponentModel
+    {
+        public string ModalId { get; set; }
+        public string HeaderText { get; set; }
+        public string FormId { get; set; }
+        public string ButtonText { get; set; }
+        public string ButtonClass { get; set; }
+        public string DetailDivId { get; set; }
+    }
+}
---
+++ CrmApplication.UI/ViewComponents/ModalViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using CrmApplication.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrmApplication.UI.ViewComponents
+{
+    public class ModalViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke(string modalId, string headerText, string formId, string buttonText, string buttonClass, string detailDivId)
+        {
+            ModalViewComponentModel model = new ModalViewComponentModel();
+            model.ModalId = modalId;
+            model.HeaderText = headerText;
+            model.FormId = formId;
+            model.ButtonText = buttonText;
+            model.ButtonClass = buttonClass;
+            model.DetailDivId = detailDivId;
+
+            return View(model);
+        }
+    }
+}
